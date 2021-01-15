8a149aecfc63663c650cce65b45bd3807b426122
Nathan Stobbs <nstobbs97@gmail.com>
2021-01-15 20:34:33 +0000

    first commit

---
+++ CustomTools.py
@@ -0,0 +1,53 @@
+import nuke
+
+def ConvertTrackerToTransform(trackerNode):
+    
+    #Checks that the node is a Tracker Node
+    nodeName = trackerNode.name()
+    
+    try:
+        nodeName.index('Tracker')
+    except ValueError:
+        nuke.message("Selected node can't be used")
+    else:
+
+        #Checks all the nodes in the Nuke Scripts, used to find the newly created node
+        setAllNodesInNukeScriptBefore = {}
+        setAllNodesInNukeScriptAfter = {}
+        setAllNodesInNukeScriptBefore = set(nuke.allNodes())
+
+        #Change the settings of the Tracker to export a Transform Node
+        trackerNode.knob("cornerPinOptions").setValue('Transform (match-move, baked)')
+        trackerNode.knob('createCornerPin').execute()
+        
+        #Finds the new transform 
+        setAllNodesInNukeScriptAfter = set(nuke.allNodes())
+        transformNode = (setAllNodesInNukeScriptAfter - setAllNodesInNukeScriptBefore) 
+        
+        #Turns on motion blur on the Transform node
+        for nodes in transformNode:
+            nodes.knob('shutteroffset').setValue('centred')
+            nodes.knob('motionblur').setValue(1)
+
+def rotoBlur():
+
+    #Creates Roto Node
+    rotoNode = nuke.createNode("Roto")
+    rotoNode.knob("cliptype").setValue("none")
+    rotoNode.knob("replace").setValue(True)
+
+    #Creates Blur Node
+    blurNode = nuke.createNode("Blur", inpanel= False)
+    blurNode.knob("size").setValue(2)
+    blurNode.knob('channels').setValue('alpha')
+
+    #Moves the blur node down for 5 units
+    blurNode.setYpos(blurNode.ypos() + 5)
+
+def buildCustomMenu():
+
+    #Creates the Custom Meun inside of Nuke's UI
+    menuBar = nuke.menu("Nuke")
+    customMenu = menuBar.addMenu("CustomTools")
+    customMenu.addCommand("Roto | Blur", "CustomTools.rotoBlur()", "o")
+    customMenu.addCommand("Convert Tracker to Transform", "CustomTools.ConvertTrackerToTransform(trackerNode = nuke.selectedNode())", "shift+t")
---
+++ menu.py
@@ -0,0 +1,7 @@
+import nuke
+import CustomTools
+# Custom Menu.py - Created on 15/1/2021 - Nuke 12.2v4 Release Build
+
+
+#Function Creates the Custom Python Menu inside Nuke's UI
+CustomTools.buildCustomMenu()
