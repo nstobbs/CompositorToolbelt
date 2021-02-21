02f002dbb78d23d9679d8ad583b60bd0a12cb780
Nathan Stobbs <nstobbs97@gmail.com>
2021-02-21 08:12:33 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,9 +1,9 @@
 import nuke
-import CustomTools
-# Custom Menu.py - Created on 15/1/2021 - Nuke 12.2v4 Release Build
+import CompositorToolbelt
+# CompositorToolbelt.py - Created on 15/1/2021 - Nuke 12.2v4 Release Build
 
 
 #Function Creates the Custom Python Menu inside Nuke's UI
-CustomTools.buildCustomMenu()
+CompositorToolbelt.buildCustomMenu()
 print('-' * 79)
 print("Compositor's Toolbelt by github.com/nstobbs")
